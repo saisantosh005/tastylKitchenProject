f29379ac72b4e227167264f828e859fb244be257
Santosh <nagasantosh.b.ib@gmail.com>
2021-09-05 04:52:30 +0000

    add desktop UI part to App

--- src/components/Authentication/LoginRoute/LoginRoute.js
+++ src/components/Authentication/LoginRoute/LoginRoute.js
@@ -1,10 +1,82 @@
 import {Component} from 'react'
+import {Cookies} from 'js-cookie'
 
-import {LoginRouteMainContainer} from './styledComponents'
+import {
+  LoginRouteMainContainer,
+  LogoAndHeadingContainer,
+  Heading,
+  LoginFormSection,
+  LoginFormMainContainer,
+  LabelAndInputContainer,
+  Label,
+  Input,
+  Button,
+  Image,
+  FormPart,
+} from './styledComponents'
+
+const loginUrl = ''
 
 class LoginRoute extends Component {
+  state = {
+    username: '',
+    password: '',
+  }
+
+  onChangeUserName = event => {
+    this.setState({
+      username: event.target.value,
+    })
+  }
+
+  onChangePassword = event => {
+    this.setState({
+      password: event.target.value,
+    })
+  }
+
+  onSubmit = async event => {
+    event.preventDefault()
+    const response = await fetch(loginUrl)
+
+    if (response.ok) {
+      const responseData = await response.json()
+      Cookies.set('jwtToken', response.responseData)
+    }
+  }
+
+  renderLoginForm = () => {
+    const {username, password} = this.state
+    return (
+      <LoginFormMainContainer onSubmit={this.onSubmit}>
+        <LogoAndHeadingContainer>
+          <Heading>Sign in</Heading>
+        </LogoAndHeadingContainer>
+        <FormPart>
+          <LabelAndInputContainer>
+            <Label>USERNAME</Label>
+            <Input value={username} onChange={this.onChangeUserName} />
+          </LabelAndInputContainer>
+          <LabelAndInputContainer>
+            <Label>PASSWORD</Label>
+            <Input value={password} onChange={this.onChangePassword} />
+          </LabelAndInputContainer>
+          <Button type="submit">Sign in</Button>
+        </FormPart>
+      </LoginFormMainContainer>
+    )
+  }
+
   render() {
-    return <LoginRouteMainContainer>Lgoin</LoginRouteMainContainer>
+    return (
+      <LoginRouteMainContainer>
+        <LoginFormSection>{this.renderLoginForm()}</LoginFormSection>
+        <Image
+          src="https://res.cloudinary.com/delguky36/image/upload/v1630816541/Rectangle_1456_1_hfd0ty.png"
+          alt=""
+        />
+      </LoginRouteMainContainer>
+    )
   }
 }
 
--- src/components/Authentication/LoginRoute/styledComponents.js
+++ src/components/Authentication/LoginRoute/styledComponents.js
@@ -1,5 +1,128 @@
 import styled from 'styled-components'
 
-const LoginRouteMainContainer = styled.div``
-const Image = styled.div``
-export {LoginRouteMainContainer, Image}
+const LoginRouteMainContainer = styled.div`
+  display: flex;
+  justify-content: center;
+
+  min-height: 100vh;
+  @media screen and (min-width: 658px) {
+    justify-content: space-around;
+  }
+`
+const LoginFormSection = styled.div`
+  width: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+const LoginFormMainContainer = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  border-radius: 8px;
+  padding: 48px;
+  @media screen and (min-width: 768px) {
+    width: 456px;
+    height: 523px;
+    box-shadow: 0px 8px 40px rgba(7, 7, 7, 0.08);
+  }
+`
+
+const LogoAndHeadingContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+const Heading = styled.h1`
+  color: #0f172a;
+  font-family: 'DM Sans';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 32px;
+  line-height: 40px;
+`
+const FormPart = styled.div``
+const LabelAndInputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 24px;
+`
+const Label = styled.label`
+  color: #475569;
+  font-family: ' DM Sans';
+  font-style: normal;
+  font-weight: bold;
+  font-size: 12px;
+  line-height: 16px;
+  text-transform: uppercase;
+  margin-bottom: 16px;
+
+  @media screen and (min-width: 658px) {
+    margin-bottom: 8px;
+  }
+`
+const Input = styled.input`
+  max-width: 360px;
+  height: 40px;
+  background: #e2e8f0;
+  border-radius: 2px;
+
+  color: #64748b;
+  font-family: ' DM Sans';
+  font-style: normal;
+  font-weight: normal;
+  font-size: 14px;
+  line-height: 24px;
+  border: none;
+  outline: none;
+  padding: 8px 16px;
+
+  @media screen and (min-width: 658px) {
+    padding: 8px 16px;
+  }
+`
+
+const Button = styled.button`
+  color: #ffffff;
+  font-family: 'DM Sans';
+  font-style: normal;
+  font-weight: bold;
+  font-size: 14px;
+  line-height: 24px;
+  max-width: 360px;
+  height: 40px;
+  background: #f7931e;
+  border-radius: 8px;
+  border: none;
+  outline: none;
+  @media screen and (min-width: 658px) {
+    max-width: 312px;
+    height: 40px;
+  }
+`
+
+const Image = styled.img`
+  display: none;
+
+  @media screen and (min-width: 658px) {
+    display: flex;
+    width: 50%;
+    height: 100vh;
+  }
+`
+
+export {
+  LoginRouteMainContainer,
+  LoginFormSection,
+  LogoAndHeadingContainer,
+  Heading,
+  FormPart,
+  Image,
+  LoginFormMainContainer,
+  LabelAndInputContainer,
+  Label,
+  Input,
+  Button,
+}
